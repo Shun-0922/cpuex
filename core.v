8d1b74c600a207382cf5308d28a4538eb9cc2260
Shun-0922 <shunsato0922@gmail.com>
2022-12-05 19:34:50 +0900

    io1

--- core.v
+++ core.v
@@ -298,3 +298,111 @@ registerfile _registerfile
 
 
 endmodule
+
+
+module instruction_out
+  (
+    output wire [1023:0] i_memory_out,
+    output wire rstn
+  );
+  assign rstn = 1'b1;
+  wire [31:0]  i_memory_input1;
+  wire [31:0]  i_memory_input2;
+  wire [31:0]  i_memory_input3;
+  wire [31:0]  i_memory_input4;
+  wire [31:0]  i_memory_input5;
+  wire [31:0]  i_memory_input6;
+  wire [31:0]  i_memory_input7;
+  wire [31:0]  i_memory_input8;
+  wire [31:0]  i_memory_input9;
+  wire [31:0]  i_memory_input10;
+  wire [31:0]  i_memory_input11;
+  wire [31:0]  i_memory_input12;
+  wire [31:0]  i_memory_input13;
+  wire [31:0]  i_memory_input14;
+  wire [31:0]  i_memory_input15;
+  wire [31:0]  i_memory_input16;
+  wire [31:0]  i_memory_input17;
+  wire [31:0]  i_memory_input18;
+  wire [31:0]  i_memory_input19;                  
+  wire [31:0]  i_memory_input20;
+  wire [31:0]  i_memory_input21;
+  wire [31:0]  i_memory_input22;
+  wire [31:0]  i_memory_input23;
+  wire [31:0]  i_memory_input24;
+  wire [31:0]  i_memory_input25;
+  wire [31:0]  i_memory_input26;
+  wire [31:0]  i_memory_input27;
+  wire [31:0]  i_memory_input28;
+  wire [31:0]  i_memory_input29;
+  wire [31:0]  i_memory_input30;
+  wire [31:0]  i_memory_input31;
+  wire [31:0]  i_memory_input32;
+  assign i_memory_out = 
+  {i_memory_input32,
+  i_memory_input31,
+  i_memory_input30,
+  i_memory_input29,
+  i_memory_input28,
+  i_memory_input27,
+  i_memory_input26,
+  i_memory_input25,
+  i_memory_input24,
+  i_memory_input23,
+  i_memory_input22,
+  i_memory_input21,
+  i_memory_input20,
+  i_memory_input19,
+  i_memory_input18,
+  i_memory_input17,
+  i_memory_input16,
+  i_memory_input15,
+  i_memory_input14,
+  i_memory_input13,
+  i_memory_input12,
+  i_memory_input11,
+  i_memory_input10,
+  i_memory_input9,
+  i_memory_input8,
+  i_memory_input7,
+  i_memory_input6,
+  i_memory_input5,
+  i_memory_input4,
+  i_memory_input3,
+  i_memory_input2,
+  i_memory_input1};
+  assign i_memory_input1 =  32'b0000000_00001_00000_000_00001_0010011;
+  assign i_memory_input2 =  32'b0000000_00001_00000_000_00010_0010011;
+  assign i_memory_input3 =  32'b0000000_00010_00000_000_00100_0010011;
+  assign i_memory_input4 =  32'b0000000_01010_00000_000_00101_0010011;
+  assign i_memory_input5 =  32'b0000000_00000_00000_000_00111_0010011;
+  assign i_memory_input6 =  32'b0000000_00001_00111_010_00000_0100011;
+  assign i_memory_input7 =  32'b0000000_00100_00111_000_00111_0010011;
+  assign i_memory_input8 =  32'b0000000_00010_00111_010_00000_0100011;
+  assign i_memory_input9 =  32'b0000000_00100_00111_000_00111_0010011;
+  assign i_memory_input10 = 32'b0000000_00010_00001_000_00011_0110011;
+  assign i_memory_input11 = 32'b0000000_00011_00111_010_00000_0100011;
+  assign i_memory_input12 = 32'b0000000_00000_00010_000_00001_0010011;
+  assign i_memory_input13 = 32'b0000000_00000_00011_000_00010_0010011;
+  assign i_memory_input14 = 32'b0000000_00001_00100_000_00100_0010011;
+  assign i_memory_input15 = 32'b0000000_00101_00100_000_01000_1100011;
+  assign i_memory_input16 = 32'b1111111_00000_00000_000_00101_1100011;
+  assign i_memory_input17 = 32'b0000000_00000_00111_010_00110_0000011;
+  assign i_memory_input18 = 32'b0000000_00001_00100_000_00100_0010011;
+  assign i_memory_input19 = 32'b1111111_00000_00000_000_11101_1100011;
+  assign i_memory_input20 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input21 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input22 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input23 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input24 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input25 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input26 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input27 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input28 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input29 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input30 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input31 = 32'b0000000_00000_00000_000_00000_0000000;
+  assign i_memory_input32 = 32'b0000000_00000_00000_000_00000_0000000;
+   
+
+endmodule
