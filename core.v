4185690134cf67a5d07cbf2d5be11fa13222cf60
Shun-0922 <shunsato0922@gmail.com>
2023-02-19 15:19:43 +0900

    only works at 50mhz

--- core.v
+++ core.v
@@ -109,7 +109,7 @@ wire [31:0] write_data_memory_ex;
 wire [31:0] write_data_memory_mem;
 wire [31:0] write_data_register_wb;
 
-assign outputs = {16'b0,output_io[15:0]};
+assign outputs = {16'b0,core_start,core_end,pc_ex[13:2],alu_ready,data_ready_mem};
 assign port_en_1_instr = 1'b1;
 
 // multiplexers etc. out of module
